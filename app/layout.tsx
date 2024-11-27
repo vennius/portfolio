3598f3f9d5c3b937c0d5974e48b3165dd2c831b3
Stevennius Chandra <stevenniuschandra@gmail.com>
2024-11-27 13:40:39 +0700

    delete unnecessarly code layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,6 @@
 import { Sora } from "next/font/google";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-const soraRegular = localFont({
-  src: "./fonts/Sora-Regular.ttf",
-  variable: "--font-sora-regular",
-  weight: "400",
-});
-
-const soraSemibold = localFont({
-  src: "./fonts/Sora-Semibold.ttf",
-  variable: "--font-sora-semibold",
-  weight: "600",
-});
-
-const soraBold = localFont({
-  src: "./fonts/Sora-Bold.ttf",
-  variable: "--font-sora-bold",
-  weight: "700",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
